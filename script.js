a836ef8cc5788ab2510d0aa0c32b4d87811da412
Михаил Ладанов <MSLadanov@gmail.com>
2023-12-06 19:11:23 +0300

    mobile navbar

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector('.burger-button')
 const shadowBox = document.querySelector('.shadow_box')
+const mobileMenu = document.querySelector('.mobile_menu')
 
 function toggleMobileMenu () {
     shadowBox.classList.toggle('active')
+    mobileMenu.classList.toggle('active-menu')
 }
 
 burger.addEventListener('click', () => toggleMobileMenu() )
